f64407afdf045534c5d51fa57f99c9b7ad159109
grAVE <g.r.a.v.e@wp.pl>
2024-05-22 15:58:46 +0200

    do cofniecia

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HistoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/history/xml', [HistoryController::class, 'index']);
