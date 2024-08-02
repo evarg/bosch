c75fdf28143ef58ead704a1d88b90ef7cbcef83c
grAVE <g.r.a.v.e@wp.pl>
2024-08-02 15:55:19 +0200

    instalations start

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\BoschFPA5000\CONFIG_NETWORK_Controller;
 use App\Http\Controllers\BoschFPA5000\MODULE_Controller;
 use App\Http\Controllers\BoschFPA5000\NODE_Controller;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\InstalationController;
 
 Route::get('/import/config', [ImportController::class, 'config']);
@@ -18,6 +19,8 @@ Route::get('/import/xsd', [xsdController::class, 'index']);
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::resource('/instalation', InstalationController::class);
+
+Route::post('/upload', [FileUploadController::class, 'upload']);
 
 Route::get('Bosch_FPA5000/CONFIG_NETWORK', [CONFIG_NETWORK_Controller::class, 'index'])->name('BoschFPA5000.CONFIG_NETWORK.index');
 Route::get('Bosch_FPA5000/NODE', [NODE_Controller::class, 'index'])->name('BoschFPA5000.NODE.index');
