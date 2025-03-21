b66339079080eda6e28530140b757df9cbaf452c
grAVE <g.r.a.v.e@wp.pl>
2025-03-21 15:48:35 +0100

    Add random user login temporary feature

---
+++ app/Http/Middleware/RandomLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class RandomLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            $randomUser = User::inRandomOrder()->first();
+            if ($randomUser) {
+                Auth::login($randomUser, true);
+            }
+        }
+
+        return $next($request);
+    }
+}
+
+
